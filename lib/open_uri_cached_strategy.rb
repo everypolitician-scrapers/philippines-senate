b926c8a0cf8d357d780a0c79e27a1d9b3df4e855
Chris Mytton <chrismytton@gmail.com>
2016-11-24 17:47:16 +0100

    Handle missing response in open-uri-cached strategy

--- lib/open_uri_cached_strategy.rb
+++ lib/open_uri_cached_strategy.rb
@@ -1,6 +1,7 @@
 class OpenURICachedStrategy < Scraped::Request::Strategy
   def response
     response = OpenUriCachedArchive.new(cache_directory).responses.find { |r| r.base_uri.to_s == url.to_s }
+    return nil if response.nil?
     { status: response.status.first.to_i, body: response.read, headers: response.meta }
   end
 
